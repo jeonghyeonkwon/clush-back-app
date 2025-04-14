f8ed742094216bd754158e669660300d0b56a946
jeonghyeonkwon <givejeong2468@gmail.com>
2025-04-14 15:12:58 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/com/clush/clushbackapp/ClushBackAppApplication.java
@@ -0,0 +1,13 @@
+package com.clush.clushbackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClushBackAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClushBackAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clush/clushbackapp/ClushBackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clush.clushbackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClushBackAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
